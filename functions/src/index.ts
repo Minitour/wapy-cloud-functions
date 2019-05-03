a71c06a94a7d0bff86abbbc2b45519e94a5cab86
Tony Zaitoun <tony.z.1711@gmail.com>
2019-05-03 12:46:10 +0300

    Firebase Auto Deploy Commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,8 +5,10 @@ import GenerateToken from './controllers/GenerateToken'
 import UpdateAccount from './controllers/UpdateAccount'
 import GetCamera from './controllers/camera/GetCamera';
 import CreateCamera from './controllers/camera/CreateCamera';
+import UpdateCamera from './controllers/camera/UpdateCamera';
 
 export const generateToken = GenerateToken;
 export const updateAccount = UpdateAccount;
 export const getCamera = GetCamera;
 export const createCamera = CreateCamera;
+export const updateCamera = UpdateCamera;
