76f037faec68edce7f7ab60125e07f478b220ad9
Tony Zaitoun <tony.z.1711@gmail.com>
2019-05-17 00:07:14 +0300

    Firebase Auto Deploy Commit

--- functions/src/controllers/camera/UpdateCamera.ts
+++ functions/src/controllers/camera/UpdateCamera.ts
@@ -6,7 +6,7 @@ import { DocumentData } from '@google-cloud/firestore';
 /**
  * 
  */
-const UpdateCamera = functions.https.onCall(async (data,context)=> {
+const UpdateCamera = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
         // Throwing an HttpsError so that the client gets the error details.
         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
@@ -17,19 +17,20 @@ const UpdateCamera = functions.https.onCall(async (data,context)=> {
     const mapModelObject = data.mmo;
     const heatmap = data.heatmap;
     const image = data.image;
+    const name = data.name;
     var store = undefined;
     const db = admin.firestore();
-    
+
     if (data.storeId) {
         store = db.doc(`stores/${data.storeId}`)
     }
     // select * from firestore.cameras where _id = cameraId and owner_uid = uid
-    
+
     try {
         const ref = db.collection('cameras').doc(cameraId);
         const doc: DocumentSnapshot = await ref.get();
         if (doc.exists) {
-            const docData = <DocumentData> doc.data();
+            const docData = <DocumentData>doc.data();
             const owner_uid = docData.owner_uid;
 
             if (owner_uid !== uid) {
@@ -37,18 +38,18 @@ const UpdateCamera = functions.https.onCall(async (data,context)=> {
                 return { status: 403, message: "The requested resource does not belong to this account." }
             }
 
-            await ref.update({mmo: mapModelObject, heatmap: heatmap,image: image, store: store})
+            await ref.update({ mmo: mapModelObject, heatmap: heatmap, image: image, store: store, name: name })
             return { status: 200, message: "Success." }
-            
+
         } else {
             // return error
             return { status: 404, message: "The requested resource does not exist." }
         }
 
-    }catch {
+    } catch {
         return { status: 400, message: "Unknown Error." }
     }
-    
+
 })
 
 export default UpdateCamera;
