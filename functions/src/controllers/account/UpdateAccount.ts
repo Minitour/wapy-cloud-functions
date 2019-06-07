ccdf297477ed6afac509e63ada580faf12638f27
Tony Zaitoun <tony.z.1711@gmail.com>
2019-06-07 17:21:46 +0300

    Firebase Auto Deploy Commit

--- functions/src/controllers/account/UpdateAccount.ts
+++ functions/src/controllers/account/UpdateAccount.ts
@@ -5,14 +5,18 @@ import * as admin from 'firebase-admin';
 const db = admin.firestore();
 
 
-const UpdateAccount = functions.https.onCall(async (data,context)=> {
+const UpdateAccount = functions.https.onCall(async (data, context) => {
     if (!context.auth) {
         // Throwing an HttpsError so that the client gets the error details.
         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
             'while authenticated.');
     }
+    console.log(data);
 
-    return await db.collection('users').doc(context.auth.uid).set(data,{merge: true});
+    const res = await db.collection('users').doc(context.auth.uid).set(data, { merge: true });
+    console.log(res);
+
+    return res;
 })
 
 export default UpdateAccount;
