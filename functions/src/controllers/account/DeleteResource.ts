d7b28bbb8cb864169e4580f6f18acd234fcfe038
Tony Zaitoun <tony.z.1711@gmail.com>
2019-06-08 01:22:59 +0300

    Firebase Auto Deploy Commit

--- functions/src/controllers/account/DeleteResource.ts
+++ functions/src/controllers/account/DeleteResource.ts
@@ -24,7 +24,7 @@ const DeleteResource = functions.https.onCall(async (data, context) => {
     const collectionName = data.from;
 
     const updatedData: any = {
-        owner_uid: "unowned"
+        owner_uid: `_${uid}`
     }
 
     try {
